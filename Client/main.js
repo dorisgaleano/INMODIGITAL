1f6c248be4ec18746ce03c4c056ca5a51ef986a7
unknown <alexpanquevac@gmail.com>
2021-11-16 13:38:37 -0500

    Se modifica imagen logo  Changes to be committed: 	modified:   Client/img/logo.jpg 	new file:   Client/img/logoPng.png 	modified:   Client/index.html 	modified:   Client/main.js 	modified:   Client/styles.css 	modified:   Server/servicioModelo.js :wq

--- Client/main.js
+++ Client/main.js
@@ -1,40 +1,42 @@
 var ngApp = angular.module('myApp', []);
-  
- ngApp.controller('myController', function ($scope, $http) {
 
-    $scope.msg = '¡Hola Inmodigital! ';
+ngApp.controller('myController', function ($scope, $http) {
 
-    $scope.getData = function () {
-                   return 'qubernet';
-                }
-                $scope.createTodo = function () {
-                    $http
-                    .post("http://localhost:9000/insertar", $scope.formData)
-                    .then(function successCallback(response)  {
-                     // $scope.formData = {};
-                      $scope.todos = response.data;
-                      console.log(response);
-                    })
-                   
-                //   alert($scope.tel+" "+$scope.cedula)
-                  };
- })
+  $scope.msg = '¡Hola Inmodigital! ';
 
 
- onload = function () {
-             document.getElementById('lbtnTest').onclick = function () {
-                 alert("Hola inmodigital")
-      // Obtenga la aplicación Angular a través del controlador
-                 var appElement = document.querySelector('[ng-controller=myController]');
-      // Obtenga la variable $ scope
-                var $scope = angular.element(appElement).scope();
-                 
-      // Llama a la variable msg y cambia el valor de msg
-                 $scope.msg = 'Trae la variable msg';
-      // La línea anterior cambió el valor de msg. Si desea sincronizar con el controlador angular, debe llamar al método $ apply ().
-                 $scope.$apply();
-      // Llame al método getData () en el controlador
-             console.log($scope.getData());
-              }
-        }
+
+  $scope.getData = function () {
+    return 'qubernet';
+  }
+  $scope.createTodo = function () {
+    $http
+      .post("http://localhost:9000/insertar", $scope.formData)
+      .then(function successCallback(response) {
+        // $scope.formData = {};
+        $scope.todos = response.data;
+        console.log(response);
+      })
+
+    //   alert($scope.tel+" "+$scope.cedula)
+  };
+})
+
+
+onload = function () {
+  document.getElementById('lbtnTest').onclick = function () {
+    alert("Hola inmodigital")
+    // Obtenga la aplicación Angular a través del controlador
+    var appElement = document.querySelector('[ng-controller=myController]');
+    // Obtenga la variable $ scope
+    var $scope = angular.element(appElement).scope();
+
+    // Llama a la variable msg y cambia el valor de msg
+    $scope.msg = 'Trae la variable msg';
+    // La línea anterior cambió el valor de msg. Si desea sincronizar con el controlador angular, debe llamar al método $ apply ().
+    $scope.$apply();
+    // Llame al método getData () en el controlador
+    console.log($scope.getData());
+  }
+}
 
--- Server/servicioModelo.js
+++ Server/servicioModelo.js
@@ -1,12 +1,12 @@
-const express= require("express");
-const {createReadStream} = require('fs')
-var modelo= require('./usuario')
+const express = require("express");
+const { createReadStream } = require('fs')
+var modelo = require('./usuario')
 
 //var qs=require("querystring")
 //var body_parser = require('body-parser');
 
 
-const app= new express();
+const app = new express();
 const HTML_CONTENT_TYPE = 'text/html'
 
 //app.use(body_parser.urlencoded({extended:true}));
@@ -22,25 +22,26 @@ app.use(bodyParser.json());
 
 
 app.use(express.static("Client"));
+app.use(express.static("node_modules"));
 
 
 const path = require("path");
-var modeloUsario= require('./inmuebles')
+var modeloUsario = require('./inmuebles')
 
 
- require('./conexion')
- //var modelo2=require('./zonas')
+require('./conexion')
+//var modelo2=require('./zonas')
 
- 
-     
- app.post("/insertar", (req, res) => {
+
+
+app.post("/insertar", (req, res) => {
   var myobj = { nombres: req.body.nombres, apellidos: req.body.apellidos, cedula: req.body.cedula, nombre_usuario: req.body.nombre_usuario, correo: req.body.correo, clave: req.body.clave, rol: req.body.rol };
-  modelo.collection.insertOne(myobj, function(err, res) {
-  if (err) throw err;
+  modelo.collection.insertOne(myobj, function (err, res) {
+    if (err) throw err;
 
   })
-  res.send("datos creados")
-  })
+  res.send("Usuario creado")
+})
 
 
 
@@ -53,26 +54,26 @@ modeloUsario.find({}, (err, docs) => {
 
 })
 */
-  //})
+//})
 
-  app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
- 
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
+
 
   createReadStream('./Client/index.html').pipe(res)
   //createReadStream('./ejercicio.html').pipe(res)
-//res.end("hola mundo");
-   
-  })
+  //res.end("hola mundo");
+
+})
 
 
-  app.listen(9000, () => {
+app.listen(9000, () => {
 
-    console.log("aplicacion corriendo en el puerto 9000")
-    
-    })
-  
-    
-   
-  
+  console.log("aplicacion corriendo en el puerto 9000")
+
+})
+
+
+
+
 
