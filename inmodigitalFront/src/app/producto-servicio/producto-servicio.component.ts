75878df147fec13c7de573ded08fe6e0710c0fba
unknown <alexpanquevac@gmail.com>
2021-12-04 08:18:26 -0500

    :wq

--- inmodigitalFront/src/app/producto-servicio/producto-servicio.component.ts
+++ inmodigitalFront/src/app/producto-servicio/producto-servicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InmuebleServiceService } from '../servicios/inmueble-service.service';
+import { UbicacionServiceService } from '../servicios/ubicacion-service.service';
 
 interface inmueblesI {
 
@@ -16,10 +17,13 @@ interface inmueblesI {
 })
 export class ProductoServicioComponent implements OnInit {
 
-  constructor(private inmuebleServicio:InmuebleServiceService) { }
+  constructor(private inmuebleServicio:InmuebleServiceService, private ubicacionServicio:UbicacionServiceService) { }
 
   inmuebles:any;
+  inmueblesRespaldo:any = [];
   inmueble:any = [];
+  ubicaciones:any = [];
+  ubicacionFiltro :any;
 
   public autenticado: boolean = false;
 
@@ -29,6 +33,13 @@ export class ProductoServicioComponent implements OnInit {
     this.inmuebleServicio.getInmuebles().subscribe(data=>{
       console.log(data)
       this.inmuebles = data
+      this.inmueblesRespaldo = data;
+    })
+
+
+    this.ubicacionServicio.getUbicaciones().subscribe(data=>{
+      console.log(data)
+      this.ubicaciones = data
     })
   }
 
@@ -42,6 +53,21 @@ export class ProductoServicioComponent implements OnInit {
 
   closePopup() {
     this.displayStyle = "none";
+  }
+
+  cambiarUbicacion(ubicacion:any){
+    this.ubicacionFiltro = ubicacion;
+    console.log(this.ubicacionFiltro);
+    this.filtrar()
+  }
+
+  filtrar(){
+    this.inmuebles = [];
+    for(let i = 0; i<= this.inmueblesRespaldo.length;i++){
+      if(this.inmueblesRespaldo[i].ubicacion == this.ubicacionFiltro._id){
+        this.inmuebles.push(this.inmueblesRespaldo[i])
+      }
+    }
   }
 
 }
